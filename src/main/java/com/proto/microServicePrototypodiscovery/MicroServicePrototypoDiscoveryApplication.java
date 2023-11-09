d8c0e7384cab85c02d9a66275257d79c498a86f6
a.turki <at.aslem@gmail.com>
2023-11-09 14:45:13 +0100

    V1 init descovery

---
+++ src/main/java/com/proto/microServicePrototypodiscovery/MicroServicePrototypoDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.proto.microServicePrototypodiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicePrototypoDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicePrototypoDiscoveryApplication.class, args);
+	}
+
+}
