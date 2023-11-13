7e07f8dfe00dc127e301566ed05f2d96302212d2
a.turki <at.aslem@gmail.com>
2023-11-13 09:13:31 +0100

    V2 config Eureka descovery server

--- src/main/java/com/proto/microServicePrototypodiscovery/MicroServicePrototypoDiscoveryApplication.java
+++ src/main/java/com/proto/microServicePrototypodiscovery/MicroServicePrototypoDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.proto.microServicePrototypodiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroServicePrototypoDiscoveryApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/com/proto/microServicePrototypodiscovery/MicroServicePrototypoDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proto.microServicePrototypodiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicePrototypoDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
